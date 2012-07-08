5ffc742e544eed9ecce4e4ecf7c983e97549bd66
iseiryu <iseiryu@ubuntu.(none)>
2012-07-08 17:50:24 -0500

    SignUp. Validation.

--- src/controller/spawnfest_security_controller.erl
+++ src/controller/spawnfest_security_controller.erl
@@ -10,14 +10,22 @@ login('POST', []) ->
     PasswordHash = mochihex:to_hex(crypto:sha256(Req:post_param("password"))),
     error_logger:info_msg("Email: ~p~nPass : ~p~n", [Email, PasswordHash]),
 
-    case boss_db:find(admin, [{email, Email}, {password_hash, PasswordHash}, {active, true}]) of
+	case boss_db:find(member, [{email, Email}, {password_hash, PasswordHash}]) of
 	[] ->
-	    boss_flash:add(SessionID, error, "Invalid username/password."),
-	    {redirect, [{action, "login"}]};
-	[Admin] ->
-	    boss_session:set_session_data(SessionID, user_id, Admin:id()),
-	    {redirect, [{controller, "main"}, {action, "index"}]}
-    end.
+		case boss_db:find(admin, [{email, Email}, {password_hash, PasswordHash}, {active, true}]) of
+		[] ->
+			boss_flash:add(SessionID, error, "Invalid username/password."),
+			{redirect, [{action, "login"}]};
+		[Admin] ->
+			is_admin = true,
+			boss_session:set_session_data(SessionID, user_id, Admin:id(), is_admin),
+			{redirect, [{controller, "main"}, {action, "index"}]}
+		end;
+	[Member] ->
+		is_admin = false,
+		boss_session:set_session_data(SessionID, user_id, Member:id(), is_admin),
+		{redirect, [{controller, "main"}, {action, "index"}]}
+	end.
 
 logout('GET', []) ->
     error_logger:info_msg("logout!!!~n"),
--- src/model/member.erl
+++ src/model/member.erl
@@ -2,3 +2,11 @@
 -compile(export_all).
 
 -belongs_to(team).
+
+validation_tests() ->
+	[{fun() -> length(Email) > 0 end,
+		"Please enter an email"},
+	{fun() -> length(PasswordHash) > 0 end,
+		"Please enter a password"},
+	{fun() -> boss_db:find(member, [{email, Email}], 1) == [] end,
+		"Please choose a different email"}].
